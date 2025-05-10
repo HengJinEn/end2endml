db276a5a600f8efd348d1b7afcfe6952045d69ab
Heng Jin En <hengh0305@gmail.com>
2025-05-10 19:18:36 +0800

    Train model

--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,13 @@ import pandas as pd
 import dill
 import os
 import sys
+
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 from src.exception import CustomException
 from src.logger import logging
 
-from src.exception import CustomException
 
 def save_object(file_path, obj):
     """
@@ -21,3 +24,34 @@ def save_object(file_path, obj):
         logging.info("Object saved successfully")
     except Exception as e:
         raise CustomException(e, sys)   
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            # gs = GridSearchCV(model,cv=3)
+            # gs.fit(X_train,y_train)
+
+            # model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
