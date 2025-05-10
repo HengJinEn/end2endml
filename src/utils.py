a071f0666b24ec113bb31f8b2b75f04728156908
Heng Jin En <hengh0305@gmail.com>
2025-05-10 21:43:09 +0800

    Deployment configuration

--- src/utils.py
+++ src/utils.py
@@ -25,20 +25,20 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)   
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            params = param[list(models.keys())[i]]
+            
+            # Perform hyperparameter tuning using GridSearchCV
+            gs = GridSearchCV(model,params, cv=3)
+            gs.fit(X_train,y_train)
 
-            # gs = GridSearchCV(model,cv=3)
-            # gs.fit(X_train,y_train)
-
-            # model.set_params(**gs.best_params_)
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
-
-            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
@@ -54,4 +54,10 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
 
     except Exception as e:
         raise CustomException(e, sys)
-    
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
